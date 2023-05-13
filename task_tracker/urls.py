c9aa33e5c6281887872f65089fc72d9e4f21e882
Burakilgar <78148646+Burakilgar@users.noreply.github.com>
2023-05-13 18:14:50 +0300

    Update urls.py

--- task_tracker/urls.py
+++ task_tracker/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import home,home2,index,delete_task
 urlpatterns = [
 
-    path('', home),
-    path('home2/', home2),
-    path('index/', index ,name="index"),
+#     path('', home),
+#     path('home2/', home2),
+    path('', index ,name="index"),
      path('delete/<int:pk>',delete_task, name="delete"),
 ]
